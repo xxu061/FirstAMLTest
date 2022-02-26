6fb44e7a4f458049242d1d4d4cef52c2875dff30
xxu061 <frank100009@hotmail.com>
2022-02-26 22:53:43 +1300

    Add order manager

---
+++ FIrstAML/FIrstAML.DomainLib/IOrderManager.cs
@@ -0,0 +1,10 @@
+﻿using FirstAML.Domain;
+using System.Collections.Generic;
+
+namespace FIrstAML.Lib
+{
+    public interface IOrderManager
+    {
+        Order GetOrder(IList<Parcel> items);
+    }
+}
---
+++ FIrstAML/FIrstAML.DomainLib/OrderManager.cs
@@ -0,0 +1,34 @@
+﻿using FirstAML.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FIrstAML.Lib
+{
+    public class OrderManager: IOrderManager
+    {
+        private readonly IParcelPriceCalculator _parcelCalculator;
+        public OrderManager(IParcelPriceCalculator parcelCalculator)
+        {
+            _parcelCalculator = parcelCalculator;
+        }
+        public Order GetOrder(IList<Parcel> items)
+        {
+            if (items == null || !items.Any())
+            {
+                throw new NullReferenceException();
+            }
+
+            Order order = new Order() { Parcels = new List<Parcel>() };
+            foreach(var item in items)
+            {
+                order.Parcels.Add(_parcelCalculator.HydrateParcelItem(item));
+            }
+
+            order.Cost = order.Parcels.Sum(p => p.TotalPrice);
+
+            return order;
+        }
+    }
+}
