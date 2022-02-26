3bc16c1f95f0ffea8b83a2df3b058df49ff542ba
xxu061 <frank100009@hotmail.com>
2022-02-26 23:42:02 +1300

    Add more unit test for calculator

---
+++ FIrstAML/FirstAM.UnitTest/ParcelCalculatorTest.cs
@@ -0,0 +1,38 @@
+using FirstAML.Domain;
+using FIrstAML.Lib;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FirstAM.UnitTest
+{
+    public class ParcelCalculatorTest
+    {
+        [Fact]
+        public async Task ShouldCalculateTotalPriceForParcel()
+        {
+            IConfiguration config = GetConfig();
+            ParcelPriceCalculator calculator = new ParcelPriceCalculator(config);
+            Parcel parcel = new Parcel() { Width = 20, Height = 20, Length = 20, Weight = 2 };
+
+            var result = await calculator.HydrateParcelItem(parcel);
+
+            result.TotalPrice.Should().Be(8);
+        }
+
+        private IConfiguration GetConfig()
+        {
+            var dictionary = new Dictionary<string, string>() {
+                {"MediumSizePrice", "8" },
+                {"MediumWeightLimit", "3" }
+            };
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(dictionary)
+                .Build();
+        }
+    }
+}
